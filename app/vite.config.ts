5088b13af9de0c0d0d7f0d4c5d69a9f92ece7552
Rommel De Leon <rommeld216@gmail.com>
2026-04-20 07:47:03 +0800

    -

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from "kimi-plugin-inspect-react"
 
 export default defineConfig({
-  base: '/CARP_Website/',
-  plugins: [inspectAttr(), react()],
+  base: "/CARP_Website/",
+  plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+})
