cfeaef5f96c0084c219624e48a3129975d20300b
Rommel De Leon <rommeld216@gmail.com>
2026-04-20 19:48:34 +0800

    Clean GitHub Pages deployment structure

--- app/src/leaflet.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'leaflet';
-declare module 'react-leaflet' {
-  export const MapContainer: any;
-  export const TileLayer: any;
-  export const Marker: any;
-  export const Popup: any;
-  export const useMap: any;
-}
